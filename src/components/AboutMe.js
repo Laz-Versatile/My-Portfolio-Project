cb8349e3973a8c98dd2acaa33e27525ddb50f4ba
Laz-Versatile <84971113+Laz-Versatile@users.noreply.github.com>
2021-09-24 22:25:29 +0100

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,10 +13,10 @@ const AboutMe = () => {
                 <div className="col-lg-6 col-xm-12">
                     <h1 className="about-heading">about me</h1>
                     <p>
-                    Hello! I am Lazarus aka Laz-Versatile from Nigeria. I am an entry-level front-end web developer and the technology I use is React JS, and also a professional graphic designer (Adobe Illustrator, PhotoShop Adobe XD, and Adobe InDesign).
-I offer a wide range of services not limited to responsive websites that are displayed on all devices desktops and smartphones, Logo design, corporate branding, product branding, content writer, and data analyst (SPSS).
-First, my client’s satisfaction is my utmost desire. Before I begin developing your website, we will discuss all the details of your niche with you. I will survey your competitors and make a list of their advantages and weaknesses.
-All this is necessary to enable me to create an appealing graphical user interface for your customers as my over five years’ experience as a graphic designer is a plus. I am all you need to kick start your organization.
+                    Hello! I am Lazarus aka Laz-Versatile from Nigeria. I am a front-end web developer and the technology I use is React JS, and also a professional graphic designer (Adobe: Illustrator, PhotoShop, XD, and InDesign).
+I offer a wide range of services not limited to responsive websites that are displayed on all devices desktops and smartphones, logo design, corporate branding, product branding, content writer, and data analysis (SPSS).
+First, my client’s satisfaction is my utmost desire. Before I begin developing your website, we will discuss all the details of your niche. I will survey your competitors and make a list of their advantages and weaknesses.
+All these are necessary to enable me to create an appealing graphical user interface for your customers as my over five years’ experience as a graphic designer is a plus. I am all you need to kick start your organization.
 Please go ahead to contact me and always feel free to ask any question as your satisfaction is my utmost desire.
 
                     </p>
